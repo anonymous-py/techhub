b44af81f80accfd56fb1963c08d0486d9aa5c2fe
cyberverx <cyberverx@gmail.com>
2025-10-25 23:47:07 +0000

    fixed the host thing

--- techhub/settings.py
+++ techhub/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@8o-qd%hbiwj8r*p3$ik4lokxynypunq-o!ajts)ygjtn#*zv#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://techhub-tfi5.onrender.com']
 
 
 # Application definition
